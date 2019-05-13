7bdf537feb27576e4a30a114152c5e0b5486eac0
vanessanaja <vanessacnaja@gmail.com>
2019-05-13 15:07:58 -0700

    refactor Box and BoxContainer components

--- src/Box.js
+++ src/Box.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
 import { choice } from './helpers';
 import './Box.css';
-
 class Box extends Component{
-  static defaultProps = {
-    allColors: ['purple', 'magenta', 'blue', 'pink']
-  };
   constructor(props){
     super(props);
-    this.state = {color: choice(this.props.allColors)};
+    this.state = {color: choice(this.props.colors)};
     this.handleClick = this.handleClick.bind(this)
   }
   pickColor(){
